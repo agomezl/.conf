684e6119bc09dbf4f550a0cf7a2ba5d3ee0ad36c
agomezl <alegomez544@gmail.com>
2015-03-26 23:15:08 -0500

    New stuff!

---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo fs.inotify.max_user_watches=100000 | sudo tee -a /etc/sysctl.conf; sudo sysctl -p
+
+sudo yum install akmod-nvidia "kernel-devel-uname-r == $(uname -r)"
+sudo yum install wget emacs gmp-devel xcompmgr feh nm-applet volumeicon \
+     xscreensaver vlc NetworkManager-vpnc-gnome scrot fail2ban aspell \
+     aspell-en aspell-es arandr broadcom-wl libXrandr-devel libX11-devel \
+     lm_sensors unrar qbittorrent libXinerama-devel libXft-devel gpicview \
+     evince
