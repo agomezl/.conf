6b2bc11d07114abd79acc5dd8f26ebf16794a409
agomezl <alegomez544@gmail.com>
2015-04-03 03:38:47 -0500

    Cabal script inprovements

--- scripts/cabal.sh
+++ scripts/cabal.sh
@@ -5,17 +5,24 @@ REAL_CABAL=/home/alien/.cabal/bin/cabal_real
 
 if [ $1 == "install" ]
 then
+    echo "###############################"
+    echo "### NOT IN A SANDBOX!! ########"
+    echo "###############################"
+    echo "Are you sure you want to install this package?"
     while true; do
-        echo "###############################"
-        echo "### NOT IN A SANDBOX!! ########"
-        echo "###############################"
-        echo "Are you sure you want to install this package?"
         read -p "(y)es / (c)reate / (n)o: " ync
         case $ync in
-            [Yy]* ) break;;
-            [Nn]* ) exit 1;;
-            [Cc]* ) cabal sandbox init;;
+            [Yy]* )
+                break;;
+            [Nn]* )
+                exit 1;;
+            [Cc]* )
+                cabal sandbox init
+                break
+                ;;
             * ) echo "Please answer create, yes or no.";;
         esac
     done
 fi
+
+${REAL_CABAL} $@
